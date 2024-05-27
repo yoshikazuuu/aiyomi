864691782d99b814843803b6f8138f64559bffc1
yoshikazuuu <febrianojerry@gmail.com>
2024-05-27 23:32:19 +0700

    chore: manga home warning

--- src/app/(manga)/manga/page.tsx
+++ src/app/(manga)/manga/page.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 export default function Manga() {
   return (
     <div className="flex min-h-[100dvh] flex-col w-screen justify-center items-center gap-2">
-      <ArrowUp />
-      <span className="text-xl max-w-xs text-center">
-        Search your manga by clicking that search tab in top or press{" "}
+      <ArrowUp size={40} className="animate-bounce text-primary" />
+      <span className="text-lg max-w-xs text-center">
+        Search your manga by clicking that search tab on top or press{" "}
         <Keys>
           <span className="text-xs">⌘</span>K
         </Keys>{" "}
